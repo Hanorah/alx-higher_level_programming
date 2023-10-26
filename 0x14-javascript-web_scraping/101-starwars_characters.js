7518072f4db77bab6afff4c44cd2d72b1fda918a
OkosodoNorah <ksdnorah@gmail.com>
2023-10-25 18:15:01 -0700

    commit

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,36 +1,21 @@
 #!/usr/bin/node
-const axios = require('axios');
-const movieId = process.argv[2];
 
-// Define the Star Wars API endpoint for movies
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
-
-// Send a GET request to the movie endpoint
-axios.get(apiUrl + movieId)
-  .then(response => {
-    if (response.status === 200) {
-      const movieData = response.data;
-      console.log("Characters in " + movieData.title + ":");
-
-      // Iterate through the characters in the same order as they appear in the 'characters' array
-      const characterPromises = movieData.characters.map(characterUrl => {
-        return axios.get(characterUrl)
-          .then(charResponse => {
-            const characterData = charResponse.data;
-            console.log(characterData.name);
-          })
-          .catch(charError => {
-            console.error('Error:', charError.message);
-          });
-      });
-
-      // Wait for all character requests to complete
-      return Promise.all(characterPromises);
-    } else {
-      console.error('Error:', response.statusText);
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-  });
-
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
