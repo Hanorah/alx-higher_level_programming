9dc0410876c5fe643f736f3badf361d44416d9be
OkosodoNorah <ksdnorah@gmail.com>
2023-10-25 17:52:21 -0700

    commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const axios = require('axios');
-const url = process.argv[2]; // Get the URL from the command-line arguments
 
-axios.get(url)
+axios.get(process.argv[2])
   .then(response => {
-    console.log('code:', response.status);
+    console.log(`code: ${response.status}`);
   })
   .catch(error => {
     console.error('Error:', error.message);
